b677ae75a1895027e15c3b8e9289c8c0ae38b423
Nick Channon <spudnyk@gmail.com>
2019-04-30 07:55:39 -0400

    better flex column style handling

--- src/components/column/createColumn.js
+++ src/components/column/createColumn.js
@@ -1,17 +1,30 @@
 import Label from './Label';
 import Text from './type/Text';
 
-const createColumnStyle = column => {
-    const { flex, width, minWidth = null, maxWidth = null, baseWidth = 0 } = column;
-
-    const style = {};
-    if (width != null) {
-        style.flexBasis = width;
-    } else {
-        style.flexGrow = flex || 1;
-        style.flexBasis = baseWidth;
-        style.flexShrink = 1;
+const flexValue = (value, emptyValue = 0, trueValue = 1, falseValue = 0) => {
+    const type = typeof value;
+    if (type === 'boolean') {
+        return value ? trueValue : falseValue;
     }
+    if (value == null) {
+        return emptyValue;
+    }
+    return value;
+};
+
+const createColumnStyle = column => {
+    const { grow, shrink, width, minWidth = null, maxWidth = null } = column;
+
+    const style = {
+        // grow by default unless a width is specified
+        flexGrow: flexValue(grow, width != null ? 0 : 1),
+        // don't shrink by default
+        flexShrink: flexValue(shrink),
+        // flex basis is 0 by default 
+        // (we don't want the css default of 'auto' or content will force cells to grow)
+        flexBasis: flexValue(width)
+    };
+
     if (maxWidth != null) {
         style.maxWidth = maxWidth;
     }
